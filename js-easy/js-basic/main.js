f9cd06d077520576859dd32197261c81c0b778f9
kanako-hg <matsushitaka65@gmail.com>
2023-11-02 09:51:22 +0900

    最初のコミットです

--- js-easy/js-basic/main.js
+++ js-easy/js-basic/main.js
@@ -1,3 +1,5 @@
+/* Step1 console.logとコンソール */
+
 console.log('Hello, world!')
 console.log('こんにちは世界!')
 console.log(1 + 2)
@@ -9,3 +11,11 @@ console.log([1, 2, 3, 4, 5]) // 配列
 console.log(true) // 真偽値
 console.log(null) // 無
 console.log({ one: 1, two: 2 }) // オブジェクト
+
+
+/* Step2 値の種類と変数 */
+
+/* answer変数に42を代入　*/
+const answer = 42
+/* answer変数を出力　*/
+console.log(answer)
